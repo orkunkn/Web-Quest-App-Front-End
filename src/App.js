149d310eadafbb2ecda0c80c3eed01400d91890f
orkunkn <orkunkaymann@gmail.com>
2023-07-07 17:56:00 +0300

    Created front-end base

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import User from './components/User/User';
+import Navbar from './components/Navbar/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar></Navbar>
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/users/:userId" element={<User />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
